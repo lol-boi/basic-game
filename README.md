418f9a73d433aaa299ec6b502766a2e81c570c4f
Anzen <amanwastakenwastaken1@gmail.com>
2023-07-05 04:57:58 +0530

    add cusotm cursor for aiming

--- README.md
+++ README.md
@@ -1,3 +1,1 @@
-This is readme file for basic game project
-this is another line in readme
-This edit by sho
+Just a basic game.
--- scripts/player.gd
+++ scripts/player.gd
@@ -4,15 +4,15 @@ extends CharacterBody2D
 @export var Bullet: PackedScene
 @export var actual_rate = 0.2
 var timer = 0
+var arrow = load("res://resources/art/arrow.png")
+
+
 
 func get_input():
 	var input_dir_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down") #gets the direction vector
 	velocity = input_dir_vector.normalized() * speed  #moves the player based on dir_vector and speed
 
-func _physics_process(delta):
-	timer +=delta
-	get_input() 
-	# condition and timming for the bullet to come out from gun 
+func shoot ():
 	if Input.get_action_raw_strength("Shoot") && timer >=actual_rate :
 		#calling the bullet node which we created
 		var me = Bullet.instantiate()
@@ -20,7 +20,17 @@ func _physics_process(delta):
 		add_sibling(me)
 		#position of the bullet
 		me.global_position = get_node("Bulletpos").get("global_position")
-		#me.set("ar_dr", (get_global_mouse_position() - self.global_position).normalized())
+		me.set("ar_dr", (get_global_mouse_position() - self.global_position).normalized())
 		timer = 0
+
+func _ready():
+	Input.set_custom_mouse_cursor(arrow)
+
+
+func _physics_process(delta):
+	timer +=delta
+	get_input() 
+	# condition and timming for the bullet to come out from gun 
+	shoot()
 	look_at(get_global_mouse_position())  # allows for the sprite to look torwards the curosr
 	move_and_slide() # not defined yet
